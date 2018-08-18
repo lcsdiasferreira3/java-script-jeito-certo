c535cd0f3468fb7632595bc9bb8667b8e10fbae1
lcsdiasferreira3 <lcsdferreira3@gmail.com>
2018-08-18 19:28:41 -0300

    Jeito certo java

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+		function teste ()   {
+	document.getElementById("demo1").innerHTML= Date();
+	}
+					
+	function myfunction ()  {
+	document.getElementById("myImage").src="html-logo-badge-512.png";
+	}
+	function myfunction2 () {
+	document.getElementById("myImage").src="js-logo-badge-512.png";
+	}
+					
